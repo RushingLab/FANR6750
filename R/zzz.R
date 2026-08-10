0ac48d84fb13c0020da664bd1856f49dabf3a276
Clark Rushing <crushing05@gmail.com>
2026-08-10 17:14:49 -0400

    Initial commit

--- R/zzz.R
+++ R/zzz.R
@@ -6,10 +6,10 @@
       # Elements in this first block aren't used directly, but are inherited
       # by others
       line = element_line(
-        size = 0.5, linetype = 1, colour = "black",
+        linewidth = 0.5, linetype = 1, colour = "black",
         lineend = "butt", color ="#f89406"
       ),
-      rect = element_rect(fill = "white", colour = "black", size = 0.5, linetype = 1),
+      rect = element_rect(fill = "transparent", colour = "black", linewidth = 0.5, linetype = 1),
       text = element_text(
         family = base_family, face = "plain", colour = "black",
         size = base_size,
@@ -33,7 +33,7 @@
         margin = margin(r = 0.8 * half_line / 2), vjust = 0.5
       ),
       axis.text.y.right = element_text(margin = margin(l = 0.8 * half_line / 2), hjust = 0),
-      axis.ticks = element_line(size = 0.2, color = "#999999"),
+      axis.ticks = element_line(linewidth = 0.2, color = "#999999"),
       axis.title.x = element_text(
         size = base_size * 1.8, vjust = 0.3,
         margin = margin(t = 10, b = 0.8 * half_line / 2)
@@ -46,7 +46,7 @@
       axis.title.y.right = element_text(angle = -90, margin = margin(l = half_line), vjust = 0),
       axis.ticks.length = grid::unit(0.3, "lines"),
 
-      legend.background = element_rect(colour = NA),
+      legend.background = element_rect(fill = "transparent", colour = NA),
       legend.spacing = unit(0.4, "cm"),
       legend.spacing.x = NULL,
       legend.spacing.y = NULL,
@@ -57,7 +57,7 @@
       legend.box.margin = margin(0, 0, 0, 0, "cm"),
       legend.box.background = element_blank(),
       legend.box.spacing = unit(0.4, "cm"),
-      legend.key = element_rect(colour = "#999999"),
+      legend.key = element_rect(fill = "transparent", colour = "#999999"),
       legend.key.size = grid::unit(1.2, "lines"),
       legend.key.height = NULL,
       legend.key.width = NULL,
@@ -70,8 +70,8 @@
       legend.justification = "center",
       legend.box = NULL,
 
-      panel.background = element_rect(colour = NA),
-      panel.border = element_rect(fill = NA, color = NA, size = .5),
+      panel.background = element_rect(fill = "transparent", colour = NA),
+      panel.border = element_rect(fill = "transparent", color = NA, linewidth = .5),
       panel.grid = element_blank(),
       panel.grid.minor = element_blank(),
       panel.spacing = grid::unit(half_line, "pt"),
@@ -80,7 +80,7 @@
       panel.spacing.y = NULL,
 
 
-      strip.background = element_rect(colour = NA),
+      strip.background = element_rect(fill = "transparent", colour = NA),
       strip.text.x = element_text(size = base_size * 1.6, margin = margin(t = half_line, b = half_line), face = "bold", color = "#999999"),
       strip.text.y = element_text(size = base_size * 1.6, angle = -90, margin = margin(l = half_line, r = half_line), face = "bold", color = "#999999"),
       strip.switch.pad.grid = unit(0.1, "cm"),
@@ -93,7 +93,7 @@
       ),
       plot.tag.position = 'topleft',
 
-      plot.background = element_rect(colour = NA),
+      plot.background = element_rect(fill = "transparent", color = NA),
       plot.title = element_text(color = "grey30",
         size = base_size * 2, hjust = 0, #face = "bold",
         vjust = 2, margin = margin(b = half_line * 1.2)
